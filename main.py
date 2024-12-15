7e9eed407aea3e3238550352bbada042adbc9fc4
marioRodriguez6 <00131819@uca.edu.sv>
2024-12-15 12:12:17 -0600

    :sparkles: main menu feature

--- main.py
+++ main.py
@@ -64,4 +64,25 @@ def add():
 
     CSV.add_entry(date, amount, category, description)
 
-CSV.get_transaction("01-01-2023", "01-01-2025")
+def main():
+    while True:
+        print("\n1. Add a new transaction")
+        print("2. View transactions and summary within a date range")
+        print("3. Exit")
+
+        choice = input("\nEnter your choice (1-3): ")
+
+        if choice == "1":
+            add()
+        elif choice == "2":
+            start_date = get_date("Enter the start date (dd-mm-YYYY): ")
+            end_date = get_date("Enter the end date (dd-mm-YYYY): ")
+            df = CSV.get_transaction(start_date, end_date)
+        elif choice == "3":
+            print("Exiting....")
+            break
+        else:
+            print("Invalid choice. Enter 1, 2 or 3")
+
+if __name__ == "__main__":
+    main()
